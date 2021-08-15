77db58c186e70125f66844ef910eb5741b1f33ce
offmarco <of.mukesh@gmail.com>
2021-08-15 15:26:28 +0530

    Q4

---
+++ DsAlgo(15-08-21).cpp
@@ -0,0 +1,23 @@
+//Date:15/08/2021//
+//Q.4//
+
+                                        /*input
+    int main()
+    {
+        // Write C++ code here
+        int ar[6] = {1, 2, 3, 4, 5, 6};
+        int sum = 0;
+        for (int i = 0; i < 6; i++)
+        {
+            sum = sum + ar[i];
+        }
+        std::cout<<"sum="<< sum;
+
+        return 0;
+    }
+
+                                        //output//
+
+    sum=21;
+
+    */
