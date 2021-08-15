09c8abb8e5357e0e73227645285db382016c5ed8
offmarco <of.mukesh@gmail.com>
2021-08-16 00:14:20 +0530

    Hollow Rectangle Pattern

---
+++ Star.cpp
@@ -0,0 +1,25 @@
+/*
+//Hollow Rectangle Pattern//
+int main() {
+    
+    for (int i=1;i<=10;i++){
+        for (int j=1;j<=10;j++){
+            if(i==1||i==10){
+                cout<<"*";
+            }
+            else{
+                if(j==1||j==10){
+                    cout<<"*";
+                }else{
+                    cout<<" ";
+                }
+            }
+        }
+        cout<<""<<endl;
+    }
+
+    return 0;
+}
+
+
+*/
