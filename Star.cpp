f761a7fbec82e822bc015deab997282f8825d615
offmarco <of.mukesh@gmail.com>
2021-08-16 23:33:23 +0530

    M print

--- Star.cpp
+++ Star.cpp
@@ -21,5 +21,27 @@ int main() {
     return 0;
 }
 
+//M print//
+for (int i=1;i<=5;i++){
+        for(int j=1;j<=5;j++){
+            if(i<=5){
+                if(j==1||j==5){
+                    cout<<"0";
+                }else if (i==1||i>3){
+                    if(1<j<5){
+                        cout<<" ";
+                    }
+                }else{
+                    if(i==j||j==i+2){
+                        cout<<"0";
+                    }else{
+                        cout<<" ";
+                    }
+                }
+            }
+        }
+        cout<<""<<endl;
+    }
+
 
 */
