e86224fd4d5927212bd8a016046b77d6d9b41d5f
x1.Mukesh <88231908+x1mukesh@users.noreply.github.com>
2021-10-01 14:16:31 +0530

    find the Median

--- MockTest1.js
+++ MockTest1.js
@@ -1,16 +1,14 @@
 
 function findMedian(arr) {
-    var arr2 = [];
+        var arr2 = [];
         for (var i = -10000; i <= 10000; i++) {
             for (var j = 0; j < arr.length; j++) {
                 if (i == arr[j]) {
                     arr2.push(arr[j]);
-                    break;
                 }
             }
         }
         var len = arr2.length;
         var m = arr2[parseInt(len/2)];
         return m;
-
 }
