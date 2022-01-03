177f8b4cc9cb89370ca29233ebf84b8d82750394
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2022-01-03 13:10:56 +0530

    optimize  code but not complete

--- taumBday.js
+++ taumBday.js
@@ -1,25 +1,8 @@
 function taumBday(b, w, bc, wc, z) {
-    if (b == w & wc == 1 & bc == 1) {
-        return b * bc + w * wc
-    }
-    else if (b < w && bc < wc) {
-        return b * bc + w * wc
-    } else {
-        var costB = b * bc
-        var costW = w * wc
-        var costBtoW = b * z
-        var costWtoB = b * z
-        var newWhite = b * wc
-        var newBlack = w * bc
-        var total = newWhite + costBtoW + costW
-        var total2 = newBlack + costWtoB + costB
-        if (total < costB + costW) {
-            return total
-        }
-        else if(total2 < costB + costW) {
-            return total2
-        }else {
-            return costB + costW
-        }
-    }
+   var costB = b * bc
+    var costW = w * wc
+    var total = costB + costW
+    var total2 = b*bc+w*(bc+z)
+    var total3 =  w*wc+b*(wc+z)
+    return Math.min(total, total2, total3)
 }
